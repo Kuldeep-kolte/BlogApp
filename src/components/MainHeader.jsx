6800c20fd3917f1486157f1d8ac189320879f126
Kuldeep Kolte <kuldeepkolate11@gmail.com>
2022-12-27 23:14:12 +0530

    Project Completed

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MainHeader() {
+    return (
+        <div className='ab1'>
+            <span className='header'>The</span>
+            <span className='h1 siren'>Siren</span>
+        </div>
+    )
+}
